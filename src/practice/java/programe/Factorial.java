2c04747b6826576909ce1bcb8791ab8cfb228acd
uroutela <uroutela@UROUTELA-LAP.oradev.oraclecorp.com>
2017-04-04 15:45:37 +0530

    q

--- src/practice/java/programe/Factorial.java
+++ src/practice/java/programe/Factorial.java
@@ -1,14 +1,18 @@
 package practice.java.programe;
 
+import java.math.BigInteger;
+
 public class Factorial {
 	public static void main(String[] args) {
-		int num=5;
-		
-		System.out.println(findFactoral(num));
+		int num=25;
+		BigInteger bi = BigInteger.valueOf(num);
+
+		System.out.println(""+ findFactoral(num));
+		System.out.println(""+ findFactoral1(bi));
 	}
 
 	private static long findFactoral(int n) {
-		
+
 		if (n == 0 || n ==1) {
 			return 1;
 		}
@@ -16,4 +20,21 @@ public class Factorial {
 			return n*findFactoral(n-1);
 		}
 	}
+
+	private static BigInteger findFactoral1(BigInteger bigInteger) {
+
+		if ( bigInteger.equals(BigInteger.ZERO) || bigInteger.equals(BigInteger.ONE)) {
+			return BigInteger.ONE;
+		}
+		else{
+			return bigInteger.multiply(findFactoral1(bigInteger.subtract(BigInteger.ONE)));
+
+		}
+	}
 }
+	
+
+
+
+
+
